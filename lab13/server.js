f619ee67f797281c18aef81ddca9d4631d200eb0
DReid98 <d.a.reid@rgu.ac.uk>
2018-03-12 15:37:31 +0000

    Express.js example

--- lab13/server.js
+++ lab13/server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req,res){
-    res.send("Hello world! by express");
+app.get('/test', function(req,res){
+    res.send("This is route 2");
 });
 
 app.listen(8080);

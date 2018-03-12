a1b46407d877512daa4382583bf32a886c2f4b02
DReid98 <d.a.reid@rgu.ac.uk>
2018-03-12 15:41:14 +0000

    Express.js example

--- lab13/server.js
+++ lab13/server.js
@@ -5,4 +5,15 @@ app.get('/test', function(req,res){
     res.send("This is route 2");
 });
 
+app.get('/', function(req,res){
+    res.send("Hello world! by express");
+});
+
+
+app.get('/joke', function(req,res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var randomJoke = oneLinerJoke.getRandomJoke();
+    res.end(randomJoke.body);
+});
+
 app.listen(8080);

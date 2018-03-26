f5457ee220588540ad175631e17bf8fd6d1e613a
DEBORAH REID (1603232) <1603232@rgu.ac.uk>
2018-03-26 16:28:43 +0100

    Fixed typo

--- lab15/server.js
+++ lab15/server.js
@@ -48,7 +48,7 @@ app.post('/search', function(req, res) {
 
     for (var i = 0; i < result.length; i++) {
       output += "<div>";
-      output += "<h3>" + result[i].name + "/h3>"
+      output += "<h3>" + result[i].name + "</h3>"
       output += "<p>" + result[i].quote + "</p>"
       output += "</div>"
     }

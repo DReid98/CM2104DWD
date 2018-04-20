0c1f70d7e1fa51e72e46176beef6520081e4918e
DReid98 <d.a.reid@rgu.ac.uk>
2018-04-20 15:39:48 +0100

    Fixing JSON file url in Ajax call

--- templates/public/js/accordion.js
+++ templates/public/js/accordion.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 
     $.ajax({
       type: "GET",
-      url: "/js/munrodata.json",
+      url: "/json/munrodata.json",
       success: function(result)
       {
         console.log(result.munros);

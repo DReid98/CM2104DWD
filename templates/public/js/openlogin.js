9855c613f088183c215297e89faafc27eb51a43e
DReid98 <d.a.reid@rgu.ac.uk>
2018-04-22 19:08:20 +0100

    Login Box - JS

--- templates/public/js/openlogin.js
+++ templates/public/js/openlogin.js
@@ -1,9 +1,9 @@
-/*
+
 //Open Login pop-up
 function openLogin() {
     $('#login').css("display","block");
 }
-
+/*
 //Close Login pop-up
 function closeLogin() {
     $('#login').css("display","none");

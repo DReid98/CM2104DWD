58827436a6a2dbabaeb4658438f6b6e45cf88a1a
DReid98 <d.a.reid@rgu.ac.uk>
2018-04-22 18:50:13 +0100

    Index page update

--- templates/public/js/openlogin.js
+++ templates/public/js/openlogin.js
@@ -8,3 +8,15 @@ function openLogin() {
 function closeLogin() {
     $('#login').css("display","none");
 }
+
+
+
+// Set modal for window click close function
+var modal = document.getElementById('login');
+
+//Close Login Pop-up on window click
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};

f7b1732648628fa15aa28b91fd2ce0e5ad0caf2d
DReid98 <d.a.reid@rgu.ac.uk>
2018-04-22 19:02:20 +0100

    Index page update

--- templates/public/js/openlogin.js
+++ templates/public/js/openlogin.js
@@ -1,4 +1,4 @@
-
+/*
 //Open Login pop-up
 function openLogin() {
     $('#login').css("display","block");
@@ -8,15 +8,25 @@ function openLogin() {
 function closeLogin() {
     $('#login').css("display","none");
 }
+*/
 
+// Close Login/Register/Password Box
+function closeBox(boxId) {
+    $('#' + boxId).css("display","none");
+}
+
+// Open Login/Register/Password Box
+function openBox(boxId) {
+    $('#' + boxId).css("display","block");
+}
 
 
 // Set modal for window click close function
-var modal = document.getElementById('login');
+var login = document.getElementById('login');
+var reg = document.getElementById('register');
+var pass = document.getElementById('forgottenPassword');
 
 //Close Login Pop-up on window click
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+    closeBox(event.target.id);
 };

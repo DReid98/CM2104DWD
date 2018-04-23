0d9184cc0af4b496d04954627b1e992779c8d3a4
DReid98 <d.a.reid@rgu.ac.uk>
2018-04-23 12:03:37 +0100

    Profile page - progress chart

--- templates/public/js/profilechart.js
+++ templates/public/js/profilechart.js
@@ -1,7 +1,7 @@
 
-// var numBagged = <%= numMunros %>;
-// numBagged = parseInt(numBagged);
-// var remaining = 282 - numBagged;
+var numBagged = <%= numMunros %>;
+numBagged = parseInt(numBagged);
+var remaining = 282 - numBagged;
 
 google.charts.load("current", {packages:["corechart"]});
 google.charts.setOnLoadCallback(drawChart);
@@ -9,16 +9,18 @@ google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
         ['Category','Number of Munros'],
-        ['Bagged',82],
-        ['Uncomplete',200]
+        ['Bagged',numBagged],
+        ['Uncomplete',remaining]
     ]);
 
     var options = {
         pieHole: 0.4,
         slices: {
-            0: {color: 'blue'},
+            0: {color: '#3B8686'},
             1: {color: 'transparent'}
-        }
+        },
+        pieSliceText: 'none',
+        legend: 'none'
     };
 
     var chart = new google.visualization.PieChart(document.getElementById('progress-donut'));
